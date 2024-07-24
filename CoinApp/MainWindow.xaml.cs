a32dee3507911e489a96d239332cc7dfe324521f
Ivan <ivansergeevich1209@gmail.com>
2024-07-24 13:28:31 +0300

    Added foo for main_window: Border_MouseLeftButtonDown (maximization and vice versa)
    Border_MouseDown (moving)
    
    + add icon for window

--- CoinApp/MainWindow.xaml.cs
+++ CoinApp/MainWindow.xaml.cs
@@ -20,5 +20,36 @@ namespace CoinApp
         {
             InitializeComponent();
         }
+
+        //Максимізація вікна
+        private bool IsMaximized = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    IsMaximized = true;
+                }
+            }
+        }
+
+        //Переміщення вікна при зажатій ЛКМ
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
     }
 }
